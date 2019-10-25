ba566d729945545c498f0ee0924e1c33173d1101
Birkir K <birkir17@ru.is>
2019-10-25 17:43:51 +0000

    logging_service

---
+++ logging_service/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.IO;
+
+namespace logging_service
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var Connection = Factory.CreateConnection())
+            using (var Channel = Connection.CreateModel())
+            {
+                var OrderExchange = "order_exchange";
+                var LoggingQueue = "logging_queue";
+                var CreateOrder = "create_order";
+
+                Channel.ExchangeDeclare(OrderExchange, "direct", true);
+                Channel.QueueDeclare(queue: LoggingQueue, true);
+                Channel.QueueBind(queue: LoggingQueue, exchange: OrderExchange, routingKey: CreateOrder);
+
+                var Consumer = new EventingBasicConsumer(Channel);
+
+                Consumer.Received += (model, ea) =>
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body);
+                    using (StreamWriter sw = File.AppendText("log.txt")) {
+                        sw.WriteLine("Log: " + message);
+                    }
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                Channel.BasicConsume(queue: LoggingQueue,
+                                 autoAck: true,
+                                 consumer: Consumer);
+
+                Channel.Close();
+                Connection.Close();
+            }
+        }
+    }
+}
